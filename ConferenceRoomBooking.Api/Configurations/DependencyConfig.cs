ccf8e410e2768ce240daf93ee004067604d8c803
zh1ganik <mykyta.zhehistovskyi@stepico.com>
2026-08-15 13:35:20 +0200

    Add SqlServer

--- ConferenceRoomBooking.Api/Configurations/DependencyConfig.cs
+++ ConferenceRoomBooking.Api/Configurations/DependencyConfig.cs
@@ -1,10 +1,15 @@
-﻿namespace ConferenceRoomBooking.Api.Configurations;
+﻿using ConferenceRoomBooking.DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceRoomBooking.Api.Configurations;
 
 public static class DependencyConfig
 {
     public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
     {
         return services
+            .AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")))
             .AddOpenApi();
     }
 }
