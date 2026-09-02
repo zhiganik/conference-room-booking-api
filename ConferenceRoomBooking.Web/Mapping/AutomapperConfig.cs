888c2c22f9245293c6afad768a28e542dd32bd11
zh1ganik <mykyta.zhehistovskyi@stepico.com>
2026-09-02 10:00:02 +0200

    Remove comment

--- ConferenceRoomBooking.Web/Mapping/AutomapperConfig.cs
+++ ConferenceRoomBooking.Web/Mapping/AutomapperConfig.cs
@@ -4,7 +4,6 @@ using ConferenceRoomBooking.Web.Dtos.Auth;
 
 namespace ConferenceRoomBooking.Web.Mapping;
 
-// Model -> Dto (and Dto -> Model) maps are added feature by feature as each controller is wired up.
 public class AutomapperConfig : Profile
 {
     public AutomapperConfig()
