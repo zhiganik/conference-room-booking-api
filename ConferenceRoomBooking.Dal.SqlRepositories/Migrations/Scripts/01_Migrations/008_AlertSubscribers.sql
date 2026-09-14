fad2a1e4bd0b18a6443451048166e8cfb50c2c9b
zh1ganik <zhehistovskii.main@gmail.com>
2026-09-14 17:59:38 +0200

    Fix sql migration

--- ConferenceRoomBooking.Dal.SqlRepositories/Migrations/Scripts/01_Migrations/008_AlertSubscribers.sql
+++ ConferenceRoomBooking.Dal.SqlRepositories/Migrations/Scripts/01_Migrations/008_AlertSubscribers.sql
@@ -4,10 +4,13 @@ BEGIN
 END
 GO
 
-CREATE TABLE [NZTelegramNotifier].[AlertSubscribers]
-(
-    [ChatId]       BIGINT       NOT NULL,
-    [CreatedAtUtc] DATETIME2(3) NOT NULL DEFAULT SYSUTCDATETIME(),
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = N'AlertSubscribers' AND schema_id = SCHEMA_ID(N'NZTelegramNotifier'))
+BEGIN
+    CREATE TABLE [NZTelegramNotifier].[AlertSubscribers]
+    (
+        [ChatId]       BIGINT       NOT NULL,
+        [CreatedAtUtc] DATETIME2(3) NOT NULL DEFAULT SYSUTCDATETIME(),
 
-    CONSTRAINT [PK_AlertSubscribers] PRIMARY KEY CLUSTERED ([ChatId])
-);
+        CONSTRAINT [PK_AlertSubscribers] PRIMARY KEY CLUSTERED ([ChatId])
+    );
+END
