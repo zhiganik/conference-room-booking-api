ea99ebba2eee1bc03e184b0fcf9bfdf18dafff1e
zh1ganik <zhehistovskii.main@gmail.com>
2026-09-14 17:43:56 +0200

    Remove webhook registration

--- ConferenceRoomBooking.Functions/Program.cs
+++ ConferenceRoomBooking.Functions/Program.cs
@@ -1,6 +1,5 @@
 using Azure.Monitor.OpenTelemetry.Exporter;
 using ConferenceRoomBooking.Bll;
-using ConferenceRoomBooking.Bll.Common.Notifications;
 using ConferenceRoomBooking.Dal.BlobsStorage;
 using ConferenceRoomBooking.Dal.SqlRepositories;
 using ConferenceRoomBooking.Dal.SqlRepositories.Migrations;
@@ -10,7 +9,6 @@ using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -33,22 +31,5 @@ if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHT
 var app = builder.Build();
 
 DatabaseMigrator.Migrate(app.Services.GetRequiredService<IConfiguration>());
-
-var hostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
-
-if (!string.IsNullOrEmpty(hostname))
-{
-    try
-    {
-        var webhookRegistrar = app.Services.GetRequiredService<IWebhookRegistrar>();
-        await webhookRegistrar.RegisterAsync($"https://{hostname}/api/webhook", CancellationToken.None);
-    }
-    catch (Exception ex)
-    {
-        app.Services.GetRequiredService<ILoggerFactory>()
-            .CreateLogger("WebhookRegistration")
-            .LogError(ex, "Failed to register the webhook");
-    }
-}
 
 app.Run();
